0104259911c1b803a3dafa7f9f69b9040dbbd54b
Tan Zhi Shan <24035177@myrp.edu.sg>
2026-01-17 21:10:47 +0800

    filter diploma

--- src/components/Diplomas.js
+++ src/components/Diplomas.js
@@ -1,14 +1,22 @@
 import { Outlet, NavLink } from "react-router-dom";
 import {getDiplomas} from "../data"
+import { useState } from "react";
 
 export default function Diplomas() {
   const diplomas = getDiplomas();
+  const [searchText, setSearchText] = useState("");
+
+  const filteredDiplomas = diplomas.filter(diploma =>
+    diploma.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+
   return (
     <div className="container">
       <h1>Schools</h1>
+      <input placeholder="Search Diplomas..." value={searchText} onChange={(e) => setSearchText(e.target.value)} />
 
       <ul className="categories">
-        {diplomas.map(cat => (
+        {filteredDiplomas.map(cat => (
           <li key={cat.id}>
             <NavLink
             className={({ isActive }) =>
